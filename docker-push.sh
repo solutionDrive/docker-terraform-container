7968f50939d0c1ba4d6756cccdd92bda6e0445b7
Tobias Lückel <tobias.lueckel@netlogix.de>
2021-09-16 13:23:40 +0200

    Fix the push command because otherwise only latest tags can be pushed

--- docker-push.sh
+++ docker-push.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Return immediately on error (with error exit status)
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push solutiondrive/terraform
+for i in `docker images --format "{{.Repository}}:{{.Tag}}" | grep "solutiondrive"`; do docker push $i; done
